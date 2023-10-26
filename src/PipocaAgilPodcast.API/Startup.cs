7ab5562a6f0b5b81521fd33fd6d00bbd27bdf0f7
RenatoPereirals <renatopreirals@gmail.com>
2023-10-26 10:25:12 -0300

    Criação do PipocaAgilPodcastContext e configurações do services.AddContex no Startup

--- src/PipocaAgilPodcast.API/Startup.cs
+++ src/PipocaAgilPodcast.API/Startup.cs
@@ -1,16 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using PipocaAgilPodcast.Persistence.Context;
 
 namespace PipocaAgilPodcast.API
 {
@@ -26,6 +16,7 @@ namespace PipocaAgilPodcast.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PipocaAgilPodcastContext>(context => context.UseNpgsql(Configuration.GetConnectionString("pipoca-server")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
