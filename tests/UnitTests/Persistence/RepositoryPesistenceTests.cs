675cf3a67396a423df0827dca063ae31267fff5f
RenatoPereirals <renatopreirals@gmail.com>
2023-11-02 21:16:54 -0300

    teste de unidade de RespositoryPersistence, Update, Delete e DeleteRange

--- tests/UnitTests/Persistence/RepositoryPesistenceTests.cs
+++ tests/UnitTests/Persistence/RepositoryPesistenceTests.cs
@@ -12,7 +12,8 @@ namespace PipocaAgilPodcast.Tests.Persistence;
     {
         // Arrange
         var mockDbContext = new Mock<PipocaAgilPodcastDbContext>();
-        mockDbContext.Setup(db => db.SaveChangesAsync(default)).Returns(Task.FromResult(1));
+        mockDbContext.Setup(db => 
+            db.SaveChangesAsync(default)).Returns(Task.FromResult(1));
 
         var repository = new RepositoryPesistence(mockDbContext.Object);
 
@@ -23,7 +24,70 @@ namespace PipocaAgilPodcast.Tests.Persistence;
         repository.SaveChangesAsync().Wait();
 
         // Assert
-        mockDbContext.Verify(db => db.SaveChangesAsync(default), Times.Once);
+        mockDbContext.Verify(db => db.Add(entity), Times.Once); // Chama o método Add
+        mockDbContext.Verify(db => db.SaveChangesAsync(default), Times.Once); // Chama o método SaveChangesAsync
+    }
+    [Fact]
+    public void Update_WithValidData_SavesToDatabase()
+    {
+        // Arrange
+        var mockDbContext = new Mock<PipocaAgilPodcastDbContext>();
+        mockDbContext.Setup(db => 
+            db.SaveChangesAsync(default)).Returns(Task.FromResult(1));
+
+        var repository = new RepositoryPesistence(mockDbContext.Object);
+
+        var entity = new UserMock();
+
+        // Act
+        repository.Update(entity);
+        repository.SaveChangesAsync().Wait();
+
+        // Assert
+        mockDbContext.Verify(db => db.Update(entity), Times.Once); // chama o método Update
+        mockDbContext.Verify(db => db.SaveChangesAsync(default), Times.Once); // Chama o método SaveChangesAsync
+    }
+    [Fact]
+    public void Delete_WithValidData_DeleteEntitiyToDatabase()
+    {
+        // Arrange
+        var mockDbContext = new Mock<PipocaAgilPodcastDbContext>();
+        mockDbContext.Setup(db => 
+            db.SaveChangesAsync(default)).Returns(Task.FromResult(1));
+
+        var repository = new RepositoryPesistence(mockDbContext.Object);
+
+        var entity = new UserMock();
+
+        // Act
+        repository.Delete(entity);
+        repository.SaveChangesAsync().Wait();
+
+        // Assert
+        mockDbContext.Verify(db => db.Remove(entity), Times.Once); // chama o método Remove
+        mockDbContext.Verify(db => db.SaveChangesAsync(default), Times.Once); // Chama o método SaveChangesAsync
+    }
+    [Fact]
+    public void DeleteRange_WithValidData_DeletesEntitiesFromDatabase()
+    {
+        // Arrange
+        var mockDbContext = new Mock<PipocaAgilPodcastDbContext>();
+        var repository = new RepositoryPesistence(mockDbContext.Object);
+
+        var entitiesToDelete = new UserMock[]
+        {
+            new UserMock { Id = 1 },
+            new UserMock { Id = 2 },
+            new UserMock { Id = 3 }
+        };
+
+        // Act
+        repository.DeleteRange(entitiesToDelete);
+        repository.SaveChangesAsync().Wait();
+
+        // Assert
+        mockDbContext.Verify(db => db.RemoveRange(entitiesToDelete), Times.Once); // chama o método RomoveRange
+        mockDbContext.Verify(db => db.SaveChangesAsync(default), Times.Once); // Chama o método SaveChangesAsync
     }
 }
 
