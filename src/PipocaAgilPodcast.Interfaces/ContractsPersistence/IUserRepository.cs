3f9e9a0365e8be2943f60a2a7bfa987ffec0be00
RenatoPereirals <renatopreirals@gmail.com>
2023-11-10 15:25:25 -0300

    Ajustes nas interaces

--- src/PipocaAgilPodcast.Interfaces/ContractsPersistence/IUserRepository.cs
+++ src/PipocaAgilPodcast.Interfaces/ContractsPersistence/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace PipocaAgilPodcast.Interfaces.ContractsPersistence;
 
     public interface IUserRepository
     {
-        Task<UserDTO> AddUsers(int userId, UserDTO model);
+        Task<UserDTO> AddUser(UserDTO model);
         Task<UserDTO> UpdateUser(int userId, UserDTO model);
-        Task<bool> DeleteUser(int userId);
+        Task<bool> DeleteUser(int userId, UserDTO model);
     }
