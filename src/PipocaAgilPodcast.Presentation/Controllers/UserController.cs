d6e3acbe8f194122bd07641403697114c7459bde
RenatoPereirals <renatopreirals@gmail.com>
2023-11-10 19:22:38 -0300

    Criação do método Post

--- src/PipocaAgilPodcast.Presentation/Controllers/UserController.cs
+++ src/PipocaAgilPodcast.Presentation/Controllers/UserController.cs
@@ -1,5 +1,7 @@
+using System.Data.Common;
 using Microsoft.AspNetCore.Mvc;
 using PipocaAgilPodcast.Application.DTOs;
+using PipocaAgilPodcast.Interfaces.ContractsPersistence;
 using PipocaAgilPodcast.Interfaces.ContractsServices;
 using PipocaAgilPodcast.Services.Error;
 
@@ -10,10 +12,13 @@ namespace PipocaAgilPodcast.Presentation.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly IUserRepository _userRepository;
 
-        public UserController(IUserService userService)
+        public UserController(IUserService userService,
+                              IUserRepository userRepository)
         {
             _userService = userService;
+            _userRepository = userRepository;
         }
 
         [HttpGet]
@@ -63,6 +68,31 @@ namespace PipocaAgilPodcast.Presentation.Controllers
             catch (Exception ex)
             {
                 return StatusCode(500, "Ocorreu um erro ao buscar o usuário por nome de usuário: " + ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UserDTO model)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+    
+                var user = await _userRepository.AddUser(model);
+
+                return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+            }
+            catch (DbException ex)
+            {
+                // Trate a violação de chave única
+                return Conflict(new { Message = $"Usuário já existe. Error {ex}" });
+
+            }
+            catch (Exception ex)
+            {
+                // Trate outras exceções
+                return StatusCode(500, new { Message = $"Erro ao processar a solicitação. Error {ex}" });
+
             }
         }
     }
