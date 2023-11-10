f56282415c3c79490db9c8682e8e758e959f576a
RenatoPereirals <renatopreirals@gmail.com>
2023-11-10 12:31:14 -0300

    Implementação da interface IuserService com o DTO

--- src/PipocaAgilPodcast.Services/Implementations/UserService.cs
+++ src/PipocaAgilPodcast.Services/Implementations/UserService.cs
@@ -6,46 +6,67 @@ using PipocaAgilPodcast.Persistence.Models;
 using PipocaAgilPodcast.Interfaces.ContractsServices;
 
 using PipocaAgilPodcast.Services.Error;
+using PipocaAgilPodcast.Application.DTOs;
+using AutoMapper;
 
 namespace PipocaAgilPodcast.Services.Implementations
 {
     public class UserService : IUserService
     { 
         public readonly PipocaAgilPodcastDbContext _context;
-        public UserService(PipocaAgilPodcastDbContext context) => _context = context;
-
-        public async Task<User[]> GetAllUsersAsync()
+        public readonly IMapper  _mapper;
+        public UserService(PipocaAgilPodcastDbContext context,
+                           IMapper mapper)
         {
-            IQueryable<User> query = _context.Users;
-            query = query.OrderBy(u => u.Id);
-
-            return await query.ToArrayAsync();
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
-        public async Task<User> GetUserByIdAsync(int userId)
-        {
-            var user = await _context.Users
-                .FirstOrDefaultAsync(u => u.Id == userId) ?? throw new UserHandlingException("Id do usuário não encontrado.");
 
-            return user;
-        }
-        public async Task<User> GetUserByUserNameAsync(string userName)
+        public async Task<UserDTO[]> GetAllUsersAsync(CancellationToken cancellationToken = default)
         {
+            IQueryable<User> query = _context.Users.OrderBy(u => u.Id);
+            var users = await query.ToListAsync(cancellationToken);
+
+            return _mapper.Map<UserDTO[]>(users);
+        }
+
+        public async Task<UserDTO> GetUserByIdAsync(int userId, CancellationToken cancellationToken = default)
+        {
+            if (userId <= 0) throw new ArgumentException("O ID do usuário deve ser maior que zero.", nameof(userId));
+        
+             var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Id == userId, cancellationToken);
+
+            if (user == null) throw new UserHandlingException("Id do usuário não encontrado.");        
+
+            return _mapper.Map<UserDTO>(user);
+        }
+
+        public async Task<UserDTO> GetUserByUserNameAsync(string userName, CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrWhiteSpace(userName)) throw new ArgumentException("O nome de usuário não pode estar vazio ou nulo.", nameof(userName));
+            
             var normalizedUserName = userName.ToLower();
     
             var user = await _context.Users
-                .FirstOrDefaultAsync(u => u.UserName.ToLower() == normalizedUserName) ?? throw new UserHandlingException("Nome de usuário não encontrado.");
+                .FirstOrDefaultAsync(u => u.UserName.ToLower() == normalizedUserName, cancellationToken);
 
-            return user;
+            if (user == null) throw new UserHandlingException("Nome de usuário não encontrado.");          
+
+            return _mapper.Map<UserDTO>(user);
         }
-        public async Task<User[]> GetUsersByInterestAsync(string interestName)
+        public async Task<UserDTO[]> GetUsersByInterestAsync(string interestName, CancellationToken cancellationToken = default)
         {
+            if (string.IsNullOrWhiteSpace(interestName)) throw new ArgumentException("O nome do interesse não pode estar vazio ou nulo.", nameof(interestName));
           
             var users = await _context.Users
-            .Include(u => u.Interests)
-            .Where(u => u.Interests != null && u.Interests.Any(i => i.Topic == interestName))
-            .ToListAsync();
+                .Include(u => u.Interests)
+                .Where(u => u.Interests != null && u.Interests.Any(i => i.Topic == interestName))
+                .ToListAsync(cancellationToken);
+            
+            if (users == null) throw new UserHandlingException("Nome de interesse não encontrados.");
 
-            return users.ToArray();           
+            return _mapper.Map<UserDTO[]>(users);           
         }  
     }
 }
